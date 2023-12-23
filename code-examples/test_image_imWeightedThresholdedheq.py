fbe716dd156d2831b21d3b578e176d2b6618e6cf
Mamdasan S <mhsaber1375@gmail.com>
2023-12-23 16:25:32 +0100

    Apply python black style 🎱

--- code-examples/test_image_imWeightedThresholdedheq.py
+++ code-examples/test_image_imWeightedThresholdedheq.py
@@ -1,19 +1,18 @@
-import numpy as np 
+import numpy as np
 from imWeightedThresholdedheq import imWTHeq
 import cv2
 import os
 
-filename = 'assets/Plane.jpg'
+filename = "assets/Plane.jpg"
 image = cv2.imread(filename)
 name, ext = os.path.splitext(filename)
 
 image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 image_v = image_hsv[:, :, 2].copy()
 
-image_v_heq, _= imWTHeq(image_v, r=0.5, v=0.5)
+image_v_heq, _ = imWTHeq(image_v, r=0.5, v=0.5)
 
 image_hsv[:, :, 2] = image_v_heq.copy()
 image_heq = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
 
-cv2.imwrite(f'{name}-imWeightedThresholdedheq{ext}', image_heq)
-
+cv2.imwrite(f"{name}-imWeightedThresholdedheq{ext}", image_heq)
--- src/imWeightedThresholdedheq.py
+++ src/imWeightedThresholdedheq.py
@@ -1,17 +1,19 @@
-import numpy as np 
+import numpy as np
 import numba
+
 
 @numba.njit()
 def imhist(image):
     hist, _ = np.histogram(image.reshape(1, -1), bins=256, range=(0, 255))
     return hist
 
+
 @numba.njit()
 def imWTHeq(image, Wout_list=np.zeros((10)), r=0.5, v=0.5):
     [h, w] = image.shape
-    PMF = imhist(image) / (h*w)
+    PMF = imhist(image) / (h * w)
     Pl = 1e-4
-    Pu = v*np.max(PMF)
+    Pu = v * np.max(PMF)
     Pwt = np.zeros_like(PMF)
     for indx, pmf in enumerate(PMF):
         if pmf < Pl:
@@ -19,28 +21,26 @@ def imWTHeq(image, Wout_list=np.zeros((10)), r=0.5, v=0.5):
         elif pmf > Pu:
             Pwt[indx] = Pu
         else:
-            Pwt[indx] = ( ((pmf - Pl) / (Pu - Pl))**r ) * Pu
-    
+            Pwt[indx] = (((pmf - Pl) / (Pu - Pl)) ** r) * Pu
+
     Cwt = np.cumsum(Pwt)
     Cwtn = Cwt / Cwt[-1]
-    Win = len(np.where(PMF>0)[0])
-    Gmax = 1.5 # 1.5 .. 2
+    Win = len(np.where(PMF > 0)[0])
+    Gmax = 1.5  # 1.5 .. 2
 
-
-    Wout = min(255, Gmax*Win)
-    if np.where(Wout_list>0)[0].size==Wout_list.size:
-        Wout = (np.sum(Wout_list)+Wout) / (1+Wout_list.size)
+    Wout = min(255, Gmax * Win)
+    if np.where(Wout_list > 0)[0].size == Wout_list.size:
+        Wout = (np.sum(Wout_list) + Wout) / (1 + Wout_list.size)
 
     F = image.copy().reshape(-1)
     Ftilde = Wout * Cwtn[F]
-    
+
     Madj = np.mean(image) - np.mean(Ftilde)
     Ftilde = Ftilde + Madj
 
-    Ftilde = np.where(Ftilde>=0, Ftilde, np.zeros_like(Ftilde))
-    Ftilde = np.where(Ftilde<=255, Ftilde, 255*np.ones_like(Ftilde))
+    Ftilde = np.where(Ftilde >= 0, Ftilde, np.zeros_like(Ftilde))
+    Ftilde = np.where(Ftilde <= 255, Ftilde, 255 * np.ones_like(Ftilde))
     Ftilde = Ftilde.astype(np.uint8)
 
     image_heq = Ftilde.reshape(h, w)
     return image_heq, Wout
-
