0a9130f64383efc2253309de3742357fc3f46f09
Mamdasan S <mhsaber1375@gmail.com>
2023-12-23 16:26:24 +0100

    Increment version number in `setup.py` + rewrite
    + apply python black style 🎱

--- setup.py
+++ setup.py
@@ -2,35 +2,43 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+with codecs.open(os.path.join(parent_dir, "README.md"), encoding="utf-8") as readme:
+    readme_description = "\n" + readme.read()
 
 
 setup(
-    name='imWeightedThresholdedheq',
-    version='0.0.5',
+    name="imWeightedThresholdedheq",
+    version="1.0.0",
     author="mamdasn s",
     author_email="<mamdassn@gmail.com>",
     url="https://github.com/Mamdasn/imWeightedThresholdedheq",
-    description='This module attempts to enhance contrast of a given image by employing a method called weighted thresholded histogram equalization (WTHE).',
-    long_description=long_description,
-    long_description_content_type = "text/markdown",
+    description="This module attempts to enhance contrast of a given image by employing a method called weighted thresholded histogram equalization (WTHE).",
+    long_description=readme_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    package_dir={'': 'src'},
+    package_dir={"": "src"},
     py_modules=["imWeightedThresholdedheq"],
     install_requires=[
-        "numpy", 
+        "numpy",
         "numba",
-        ],
-    keywords=['python', 'histogram', 'HE', 'hist equalization', 'histogram based equalization', 'weighted thresholded histogram equalization', 'contrast enhancement'],
+    ],
+    keywords=[
+        "python",
+        "histogram",
+        "HE",
+        "hist equalization",
+        "histogram based equalization",
+        "weighted thresholded histogram equalization",
+        "contrast enhancement",
+    ],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
-    scripts=['bin/imWeightedThresholdedheq'],
+    scripts=["bin/imWeightedThresholdedheq"],
 )
